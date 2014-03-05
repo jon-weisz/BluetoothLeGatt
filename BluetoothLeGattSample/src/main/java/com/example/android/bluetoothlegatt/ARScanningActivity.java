d368ed3627dfd505df56d6a428a51b6426d07917
jon-weisz <jweisz@cs.columbia.edu>
2014-03-05 14:52:13 -0500

    fleshed out ARScanningActivity

--- BluetoothLeGattSample/src/main/java/com/example/android/bluetoothlegatt/ARScanningActivity.java
+++ BluetoothLeGattSample/src/main/java/com/example/android/bluetoothlegatt/ARScanningActivity.java
@@ -1,37 +1,61 @@
 package com.example.android.bluetoothlegatt;
 
 import android.app.Activity;
+import android.util.Log;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ARScanningActivity extends Activity {
+import java.lang.reflect.Array;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_arscanning);
-    }
+import edu.dhbw.andar.ARToolkit;
+import edu.dhbw.andar.AndARActivity;
+import edu.dhbw.andar.exceptions.AndARException;
 
+public class ARScanningActivity extends AndARActivity {
+    ARToolkit artoolkit;
+    List<LocalizationMarker> localizationMarkers;
+    private final static List<String> MARKERLIST =
+            Collections.unmodifiableList(Arrays.asList("android.patt, patt.hiro, barcode.patt"));
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.arscanning, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public void addLocalizationMarker(String markerName){
+        LocalizationMarker newMarker;
+        newMarker = new LocalizationMarker(markerName, markerName, 10.f, new double[]{0,0});
+        localizationMarkers.add(newMarker);
+        try{
+            artoolkit.registerARObject(newMarker);
         }
-        return super.onOptionsItemSelected(item);
+        catch(AndARException ex)
+            {
+               Log.e("LocalizationMarker:Failed to load localization marker - name:" +markerName,
+                       ex.getMessage());
+            }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_arscanning);
+        artoolkit = super.getArtoolkit();
+
+        for( String lmName : MARKERLIST)
+        {
+            addLocalizationMarker(lmName);
+        }
+
+        startPreview();
+
+    }
+
+
+    @Override
+     public void uncaughtException(Thread thread, Throwable ex)
+    {
+        Log.e("AndAR Exception", ex.getMessage());
+        finish();
     }
 
 }
